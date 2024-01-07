cb8178be37053846e4da86c1e90612f16e7c2010
Rusca <rusca.dev@gmail.com>
2024-01-07 20:43:05 +0100

    catagrama beta - update test tweak

--- cata_update.py
+++ cata_update.py
@@ -2,4 +2,7 @@ import catagrames as cg
 
 print("Actualitzant la frase del catagrama... (de mentida, de moment)")
 
-cg.add_new_quote_to_archive()
+try:
+    cg.add_new_quote_to_archive()
+except:
+    print("No ha funcionat la cosa")
